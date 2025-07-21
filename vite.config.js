9c2de050263b284d901c9bd41222547bdf232315
Adam Gurley <adam.gurley@live.com>
2025-07-20 18:08:46 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: ['*'] // ✅ Allow all hosts
+  },
+  server: {
+    host: true
+  }
 });
