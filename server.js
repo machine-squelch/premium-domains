0ab995f7dad080f1af4b1d610abb1c997fcaee65
Adam Gurley <adam.gurley@live.com>
2025-07-20 19:03:24 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ import { fileURLToPath } from 'url';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 
-app.use(express.static(join(__dirname, 'dist'), { maxAge: '1y' }));
+app.use(express.static(join(__dirname, 'dist')));
 
-app.get('*', (_, res) => {
-  res.sendFile(join(__dirname, 'dist', 'index.html'));
-});
+app.get('*', (_, res) => res.sendFile(join(__dirname, 'dist', 'index.html')));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`✅ Server running on port ${port}`));
+
+app.use(express.static(join(__dirname, 'dist')));
+app.get('*', (_, res) => res.sendFile(join(__dirname, 'dist', 'index.html')));
