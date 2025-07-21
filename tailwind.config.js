49bf1b4a1be8884a7b91bd93ffc59239b7878546
Adam Gurley <adam.gurley@live.com>
2025-07-20 17:25:58 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        premiumStart: "#ff00ff",
+        premiumEnd: "#00f7ff",
+        midStart: "#00c6ff",
+        midEnd: "#0072ff",
+        modStart: "#aaa",
+        modEnd: "#eee",
+      },
+    },
+  },
+  plugins: [],
+};
