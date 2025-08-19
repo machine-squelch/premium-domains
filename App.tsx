c2484749dbbfe87c0f5073693317a7f84fab49ec
Adam Gurley <adam.gurley@live.com>
2025-08-19 07:42:18 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -121,7 +121,7 @@ export default function App() {
             ))}
           </div>
         </section>
-      </div>
+     <h2 className="text-3xl font-bold mb-6 text-cyan-400">COPYRUGHT 2025 - THINKAZOO - ALL RIGHTS RESERVED</h2>
     </div>
   );
 }
